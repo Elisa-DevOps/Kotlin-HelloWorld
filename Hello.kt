c5b4edbb8f62f629fdad8f010280a58972543d2a
Elisa <elisa-devops@users.noreply.github.com>
2024-09-29 08:04:55 -0500

    first single line comment

--- Hello.kt
+++ Hello.kt
@@ -1,5 +1,5 @@
 fun main() {
-  println("Hello, World!")
+  println("Hello, World!")      //first comment: code prints to the terminal
   print(15*4);
   print ("This sententence");
   print ("is missing a space.");
