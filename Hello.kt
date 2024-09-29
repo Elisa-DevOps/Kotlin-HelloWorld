de9f6cdc6481a7d5171db795a685827f9883ba53
Elisa <elisa-devops@users.noreply.github.com>
2024-09-29 09:25:21 -0500

    corrected the spaces

--- Hello.kt
+++ Hello.kt
@@ -1,8 +1,8 @@
 fun main() {
   println("Hello, World!")      //first comment: code prints to the terminal
   print(15*4);
-  print ("This sententence");       //sentence is spelled incorrectly
-  print ("is missing a space.");
+  print("This sententence");       //sentence is spelled incorrectly
+  print("is missing a space.");
 
    /* Here lies
     a multline comment 
